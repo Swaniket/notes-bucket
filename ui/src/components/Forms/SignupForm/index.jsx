cbf0fb1fe93c5cb2d3fcf75574f30bbef5c5653f
Swaniket <swaniketchowdhury@gmail.com>
2023-03-17 22:37:52 +0530

    UI - Refactoring Signup component, added password policy tooltip

---
+++ ui/src/components/Forms/SignupForm/index.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+import { Col, Row, Form, OverlayTrigger, Popover } from "react-bootstrap";
+import { FaInfoCircle } from "react-icons/fa";
+
+function SignupForm({ values, errors, touched, handleBlur, handleChange }) {
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h5">Password Policy</Popover.Header>
+      <Popover.Body>
+        <strong>Password Must be</strong>
+        <ul>
+          <li>Minimum eight characters</li>
+          <li>At least one uppercase letter</li>
+          <li>At least one lowercase letter</li>
+          <li>At least one number</li>
+          <li>At least one special character</li>
+        </ul>
+      </Popover.Body>
+    </Popover>
+  );
+
+  const PasswordPolicy = () => {
+    return (
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ paddingLeft: "7px" }}>
+          <FaInfoCircle />
+        </span>
+      </OverlayTrigger>
+    );
+  };
+
+  return (
+    <>
+      <Row>
+        <Col>
+          {/* Firstname */}
+          <Form.Group className="mb-3">
+            <Form.Label className="field-header">
+              <strong>Enter Your Firstname</strong>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Jhon"
+              id="firstName"
+              name="firstName"
+              autoComplete="off"
+              value={values.firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isInvalid={touched.firstName && !!errors.firstName}
+              isValid={touched.firstName && !errors.firstName}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              {errors.firstName}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+        <Col>
+          {/* Lastname */}
+          <Form.Group className="mb-3">
+            <Form.Label className="field-header">
+              <strong>Enter Your Lastname</strong>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Doe"
+              id="lastName"
+              name="lastName"
+              autoComplete="off"
+              value={values.lastName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              isInvalid={touched.lastName && !!errors.lastName}
+              isValid={touched.lastName && !errors.lastName}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              {errors.lastName}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+      </Row>
+
+      {/* Email */}
+      <Form.Group className="mb-3">
+        <Form.Label className="field-header">
+          <strong>Enter Your Email</strong>
+        </Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="name@domain.com"
+          id="email"
+          name="email"
+          autoComplete="off"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          isInvalid={touched.email && !!errors.email}
+          isValid={touched.email && !errors.email}
+        />
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          {errors.email}
+        </Form.Control.Feedback>
+      </Form.Group>
+      {/* <br /> */}
+      <hr />
+      {/* <br /> */}
+      {/* Password */}
+      <Form.Group className="mb-3">
+        <Form.Label className="field-header">
+          <strong>Choose a Password</strong>
+
+          <PasswordPolicy />
+        </Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="choose a strong password"
+          id="password"
+          name="password"
+          autoComplete="off"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          isInvalid={touched.password && !!errors.password}
+          isValid={touched.password && !errors.password}
+        />
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          {errors.password}
+        </Form.Control.Feedback>
+      </Form.Group>
+      {/* Confirm Password */}
+      <Form.Group className="mb-3">
+        <Form.Label className="field-header">
+          <strong>Confirm Password</strong>
+        </Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="retype your password"
+          id="confirmPassword"
+          name="confirmPassword"
+          autoComplete="off"
+          value={values.confirmPassword}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          isInvalid={touched.confirmPassword && !!errors.confirmPassword}
+          isValid={touched.confirmPassword && !errors.confirmPassword}
+        />
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          {errors.confirmPassword}
+        </Form.Control.Feedback>
+      </Form.Group>
+    </>
+  );
+}
+
+export default SignupForm;
--- ui/src/pages/Signup/index.jsx
+++ ui/src/pages/Signup/index.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Button, Form } from "react-bootstrap";
 import { useFormik } from "formik";
-import { Col, Row, Button, Form } from "react-bootstrap";
 import { FaUserPlus } from "react-icons/fa";
+import { SignupForm } from "../../components";
 import { signUpSchema } from "./Schema";
 import "./index.css";
 
@@ -32,8 +33,8 @@ function Signup() {
     navigate("/");
   };
 
-  return (
-    <div className="custom-container">
+  const SignupHeader = () => {
+    return (
       <section>
         <h1 className="heading">
           <FaUserPlus style={{ margin: "5px" }} /> Sign Up
@@ -43,127 +44,12 @@ function Signup() {
           Enter your details to Sign Up
         </p>
       </section>
+    );
+  };
 
-      <Form noValidate onSubmit={handleSubmit}>
-        <Row>
-          <Col>
-            {/* Firstname */}
-            <Form.Group className="mb-3">
-              <Form.Label className="field-header">
-                <strong>Enter Your Firstname</strong>
-              </Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Jhon"
-                id="firstName"
-                name="firstName"
-                autoComplete="off"
-                value={values.firstName}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                isInvalid={touched.firstName && !!errors.firstName}
-                isValid={touched.firstName && !errors.firstName}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              <Form.Control.Feedback type="invalid">
-                {errors.firstName}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Col>
-          <Col>
-            {/* Lastname */}
-            <Form.Group className="mb-3">
-              <Form.Label className="field-header">
-                <strong>Enter Your Lastname</strong>
-              </Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Doe"
-                id="lastName"
-                name="lastName"
-                autoComplete="off"
-                value={values.lastName}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                isInvalid={touched.lastName && !!errors.lastName}
-                isValid={touched.lastName && !errors.lastName}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-              <Form.Control.Feedback type="invalid">
-                {errors.lastName}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Col>
-        </Row>
-
-        {/* Email */}
-        <Form.Group className="mb-3">
-          <Form.Label className="field-header">
-            <strong>Enter Your Email</strong>
-          </Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="name@domain.com"
-            id="email"
-            name="email"
-            autoComplete="off"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            isInvalid={touched.email && !!errors.email}
-            isValid={touched.email && !errors.email}
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            {errors.email}
-          </Form.Control.Feedback>
-        </Form.Group>
-        {/* Password */}
-        <Form.Group className="mb-3">
-          <Form.Label className="field-header">
-            <strong>Choose a Password</strong>
-          </Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="choose a strong password"
-            id="password"
-            name="password"
-            autoComplete="off"
-            value={values.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            isInvalid={touched.password && !!errors.password}
-            isValid={touched.password && !errors.password}
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            {errors.password}
-          </Form.Control.Feedback>
-        </Form.Group>
-        {/* Confirm Password */}
-        <Form.Group className="mb-3">
-          <Form.Label className="field-header">
-            <strong>Confirm Password</strong>
-          </Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="retype your password"
-            id="confirmPassword"
-            name="confirmPassword"
-            autoComplete="off"
-            value={values.confirmPassword}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            isInvalid={touched.confirmPassword && !!errors.confirmPassword}
-            isValid={touched.confirmPassword && !errors.confirmPassword}
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          <Form.Control.Feedback type="invalid">
-            {errors.confirmPassword}
-          </Form.Control.Feedback>
-        </Form.Group>
-
-        {/* Buttons */}
+  const SignupButtons = () => {
+    return (
+      <>
         <div className="button-group-login-signup">
           <section>
             <Button className="btn btn-light" onClick={navigateToLogin}>
@@ -181,6 +67,22 @@ function Signup() {
             </Button>
           </section>
         </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="custom-container">
+      <SignupHeader />
+      <Form noValidate onSubmit={handleSubmit}>
+        <SignupForm
+          values={values}
+          errors={errors}
+          touched={touched}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+        />
+        <SignupButtons />
       </Form>
     </div>
   );
